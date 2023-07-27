065e5923d8b9900a5c31cf62b917cbc1b0bb608d
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-07-27 10:00:42 +0300

    7. Аннотация @Override [#174891 #431511]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -2,10 +2,6 @@ package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
 
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
     @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
