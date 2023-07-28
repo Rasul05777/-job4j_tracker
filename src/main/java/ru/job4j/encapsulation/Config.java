1fd3aea7e838af19d00d923a5c7f3d79142735f9
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-07-28 15:04:05 +0300

    0. Что такое инкапсуляция. [#3818]

--- src/main/java/ru/job4j/encapsulation/Config.java
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -5,16 +5,18 @@ public class Config {
     private int position;
     private String[] properties;
 
-    public  Config(String name) {
+    public Config(String name) {
         this.name = name;
     }
+
     public void print() {
         System.out.println(position);
     }
 
-    public  String getProperty(String key) {
+    public String getProperty(String key) {
         return search(key);
     }
+
     public String search(String key) {
         return key;
     }
