3c8e8f7082741a8ee5a69345cf39ce28807908ba
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-07-31 12:51:32 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+import java.util.Date;
+import ru.job4j.pojo.Student;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Хизриев Магомедрасул Заурович");
+        student.setGroup(4);
+        student.setReceipt(new Date());
+
+        System.out.println("ФИО : " + student.getFullName() + ", группа : " + student.getGroup() + ", дата поступления : " + student.getReceipt());
+    }
+}
