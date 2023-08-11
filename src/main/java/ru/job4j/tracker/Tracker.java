15d7072b8b166379f053c81a699db93aba021dce
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-08-11 19:14:39 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -21,7 +21,7 @@ public class Tracker {
         Item[] rsl = new Item[size];
         int counter = 0;
         for (int i = 0; i < size; i++) {
-            if(items[i].getName().equals(key)) {
+            if (items[i].getName().equals(key)) {
                 rsl[counter] = items[i];
                 counter++;
             }
@@ -30,11 +30,26 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+    int index = indexOf(id);
+    if(index != -1) {
+        item.setId(id);
+        items[index] = item;
+        return true;
+    } else {
+        return false;
+    }
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < size; i++) {
+            if(items[i].getId() == id) {
+                rsl = i;
                 break;
             }
         }
