0dedffcccdc2dba11c7e3eedc1bbb3dcde8d746b
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-09-19 13:04:30 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -20,13 +25,19 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new AllAction(), new ReplaceAction(), new DeleteAction(),
-                new FindIdAction(), new FindNameIdAAction(), new ExitAction()
+                new CreateAction(output),
+                new AllAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindIdAction(),
+                new FindNameIdAAction(),
+                new ExitAction(),
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
 
