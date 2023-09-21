16b875b916672d63b8328f15a87a62fb6f0f014d
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-09-21 16:51:22 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Factorial {
+    public int calc(int a) {
+        if(a < 0) {
+            throw new IllegalArgumentException("A could not be less than 0");
+        }
+        int rsl = 1;
+        for (int index = 2; index <= a; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int result = new Factorial().calc(8);
+        System.out.println(result);
+    }
+}
