2270cb824847fab2b8f5477c7b64641cf4c59186
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-10-07 15:02:33 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person people : persons) {
+            if (people.getName().contains(key) || people.getSurname().contains(key)
+                    || people.getAddress().contains(key) || people.getPhone().contains(key)) {
+                result.add(people);
+            }
+        }
+        return result;
+    }
+
+}
