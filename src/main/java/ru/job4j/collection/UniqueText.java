b00729069196976e2435d1011d2acd8886719014
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-10-20 22:59:43 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String origText, String duplicateText) {
+        String[] origin = origText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> chek = new HashSet<>();
+        for(String word : origin) {
+            chek.add(word);
+        }
+        for (String words : text) {
+            if(!chek.contains(words)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
