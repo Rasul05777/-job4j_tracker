71b1b20603de90467ca926d05d9cf4c336cae251
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-11-08 15:24:37 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/main/java/ru/job4j/collection/Job.java
@@ -0,0 +1,52 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Job implements Comparator<Job> {
+    private String name;
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{"
+                + "name='" + name + '\''
+                + ", priority=" + priority
+                + '}';
+    }
+
+    @Override
+    public int compare(Job o1, Job o2) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Job job = (Job) o;
+        return priority == job.priority && Objects.equals(name, job.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, priority);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,90 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+    @Test
+    public void whenJobAscByPriority() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Rsl", 5));
+        list.add(new Job("Senior", 3));
+        list.add(new Job("Dev", 2));
+        Collections.sort(list, new JobAscByPriority());
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Dev", 2));
+        expected.add(new Job("Senior", 3));
+        expected.add(new Job("Rsl", 5));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenJobDescByPriority() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Rsl", 5));
+        list.add(new Job("Senior", 3));
+        list.add(new Job("Dev", 2));
+        Collections.sort(list, new JobDescByPriority());
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Rsl", 5));
+        expected.add(new Job("Senior", 3));
+        expected.add(new Job("Dev", 2));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenJobAscBynName() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Rsl", 5));
+        list.add(new Job("Senior", 3));
+        list.add(new Job("Dev", 2));
+        Collections.sort(list, new JobAscName());
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Dev", 2));
+        expected.add(new Job("Rsl", 5));
+        expected.add(new Job("Senior", 3));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenJobDescBynName() {
+        List<Job> list = new ArrayList<>();
+        list.add(new Job("Rsl", 5));
+        list.add(new Job("Senior", 3));
+        list.add(new Job("Dev", 2));
+        Collections.sort(list, new JobDescByName());
+        List<Job> expected = new ArrayList<>();
+        expected.add(new Job("Senior", 3));
+        expected.add(new Job("Rsl", 5));
+        expected.add(new Job("Dev", 2));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void whenComparatorJobAscByNameAndAscPriority() {
+        Comparator<Job> comparator = new JobAscName().thenComparing(new JobAscByPriority());
+        int rsl = comparator.compare(
+                new Job("Rsl", 0),
+                new Job("Senior", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorJobDescByNameAndDescPriority() {
+        Comparator<Job> comparator = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = comparator.compare(
+                new Job("Senior", 1),
+                  new Job("Rsl", 0)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+}
