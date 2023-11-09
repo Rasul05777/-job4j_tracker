85ef0fc1a8d4ddb971e74a8a86b82ee4b78bc5d7
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-11-09 13:49:47 +0300

    2. Комбинированный компаратор [#10036]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -73,7 +73,7 @@ public class JobTest {
         Comparator<Job> comparator = new JobAscName().thenComparing(new JobAscByPriority());
         int rsl = comparator.compare(
                 new Job("Rsl", 0),
-                new Job("Senior", 1)
+                new Job("Rsl", 1)
         );
         assertThat(rsl).isLessThan(0);
     }
@@ -83,7 +83,7 @@ public class JobTest {
         Comparator<Job> comparator = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = comparator.compare(
                 new Job("Senior", 1),
-                  new Job("Rsl", 0)
+                  new Job("Senior", 0)
         );
         assertThat(rsl).isLessThan(0);
     }
