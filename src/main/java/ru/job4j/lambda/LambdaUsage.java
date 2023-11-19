7ad6670d7856c9b7a434b962d9838c238e6fb716
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-11-19 15:09:06 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,20 +1,32 @@
 package ru.job4j.lambda;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.*;
 
 public class LambdaUsage {
     public static void main(String[] args) {
-        List<String> strings = Arrays.asList("eeee", "a", "ccc", "dddd", "bb");
-        Comparator<String> comparator  = (left, right) -> {
-            System.out.println("comparator - " + left.length() + " : " + right.length());
-            return  Integer.compare(right.length(), left.length());
-        };
-        strings.sort(comparator);
-        for (String str : strings) {
-            System.out.println(str);
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (key, value) -> map.put(key, value);
+            biCon.accept(1, "one");
+            biCon.accept(2, "two");
+            biCon.accept(3, "three");
+            biCon.accept(4, "four");
+            biCon.accept(5, "five");
+            biCon.accept(6, "six");
+            biCon.accept(7, "seven");
+        BiPredicate<Integer, String> biPred = (s, s1) -> (s % 2 == 0) || (s1.length() == 4);
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = value -> value.toUpperCase();
+        for (String s : strings) {
+            con.accept(func.apply(s));
         }
     }
 }
